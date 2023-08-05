529e667ca45d1b85f2b76fa11125281179bb4464
pawelok <pawelok35@gmail.com>
2023-08-05 15:14:23 +0200

    exercise 2

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,54 @@
-{
-  ('use strict');
-  
-  function renderBooks() {
-    const booksList = document.querySelector('.books-list');
-    const bookTemplate = Handlebars.compile(document.getElementById('template-book').innerHTML);
-  
-    for (const book of dataSource.books) {
-      // Wygeneruj kod HTML na podstawie szablonu oraz danych o konkretnej książce
-      const bookHTML = bookTemplate(book);
-  
-      // Utwórz element DOM z kodu HTML przy użyciu utils.createDOMFromHTML()
-      const bookElement = utils.createDOMFromHTML(bookHTML);
-  
-      // Dołącz wygenerowany element DOM jako nowe dziecko DOM do listy .books-list
-      booksList.appendChild(bookElement);
-    }
+('use strict');
+
+// Pusta tablica na ulubione książki
+const favoriteBooks = [];
+
+function renderBooks() {
+  const booksList = document.querySelector('.books-list');
+  const bookTemplate = Handlebars.compile(document.getElementById('template-book').innerHTML);
+
+  for (const book of dataSource.books) {
+    const bookHTML = bookTemplate(book);
+    const bookElement = utils.createDOMFromHTML(bookHTML);
+
+    booksList.appendChild(bookElement);
   }
-  renderBooks();
 }
+
+// Funkcja inicjująca nasłuchiwanie na dwuklik na elementach .book__image
+function initActions() {
+  const bookImages = document.querySelectorAll('.book__image');
+
+  // Przejdź przez każdy element .book__image i dodaj nasłuchiwacz dla dwukliku (dblclick)
+  for (const bookImage of bookImages) {
+    bookImage.addEventListener('dblclick', function (event) {
+      event.preventDefault();
+
+      // Dodaj klasę 'favorite' do klikniętego elementu
+      bookImage.classList.toggle('favorite');
+
+      // Pobierz identyfikator książki z atrybutu data-id klikniętego elementu
+      const bookId = bookImage.getAttribute('data-id');
+
+      // Sprawdź, czy identyfikator książki znajduje się już w tablicy favoriteBooks
+      const isFavorite = favoriteBooks.includes(bookId);
+
+      // Jeśli książka nie jest jeszcze w tablicy favoriteBooks, dodaj ją
+      if (!isFavorite) {
+        favoriteBooks.push(bookId);
+      } else {
+        // W przeciwnym razie usuń ją z tablicy
+        const index = favoriteBooks.indexOf(bookId);
+        if (index !== -1) {
+          favoriteBooks.splice(index, 1);
+        }
+      }
+    });
+  }
+}
+
+// Wywołaj funkcję renderBooks, aby wyrenderować książki na stronie
+renderBooks();
+
+// Wywołaj funkcję initActions, aby dodać nasłuchiwanie na dwuklik na elementach .book__image
+initActions();
